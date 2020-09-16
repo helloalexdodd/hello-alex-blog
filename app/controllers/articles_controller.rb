270ad4e3c994e8a1738ce055f0191d665b23471c
helloalexdodd <hello@alexdodd.ca>
2020-09-16 16:37:48 -0400

    added show methods for individual articles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,6 @@
+class ArticlesController < ApplicationController
+  def show
+    # the @ turns this variable into an instance variable, making it accessible outside of the show method
+    @article = Article.find(params[:id])
+  end
+end
