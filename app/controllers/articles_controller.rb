18724a92348664e724ed92230354ddbf800ca072
helloalexdodd <hello@alexdodd.ca>
2020-09-16 16:50:08 -0400

    add all articles list page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,7 @@ class ArticlesController < ApplicationController
     # the @ turns this variable into an instance variable, making it accessible outside of the show method
     @article = Article.find(params[:id])
   end
+  def index
+    @articles = Article.all
+  end
 end
