0e82554f2c6912a9232ec3871226fe7640dde8c0
helloalexdodd <hello@alexdodd.ca>
2020-09-16 18:30:45 -0400

    editing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,15 +3,36 @@ class ArticlesController < ApplicationController
     # the @ turns this variable into an instance variable, making it accessible outside of the show method
     @article = Article.find(params[:id])
   end
+
   def index
     @articles = Article.all
   end
+  
   def new
-
+    @article = Article.new
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    if @article.save
+      flash[:notice] = 'Article was created successfully'
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = 'Article was created successfully'
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 end
