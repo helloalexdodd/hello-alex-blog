35de51b9c91d7257f917e5699181b47cd96baeb1
helloalexdodd <hello@alexdodd.ca>
2020-09-17 09:09:49 -0400

    adding partials and refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-    # the @ turns this variable into an instance variable, making it accessible outside of the show method
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -13,11 +12,10 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
   
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = 'Article was created successfully'
       redirect_to @article
@@ -27,8 +25,7 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = 'Article was created successfully'
       redirect_to @article
     else
@@ -37,8 +34,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+  # everything below this is available only to this class
+  private
+
+  def set_article
+    # the @ turns this variable into an instance variable, making it accessible outside of the show method
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
