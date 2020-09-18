28eae65463b8fe5d9c059b574bb1aef74d2f8d10
helloalexdodd <hello@alexdodd.ca>
2020-09-18 00:09:20 -0400

    signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # since we are using every route, we can remove the only specification
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :articles
+  get 'signup', to: 'users#new'
+  # we can individually declare routes, or instead just open all routes, with exceptions
+  # post 'user', to: 'users#create'
+  resources :users, except: [:new]
 end
